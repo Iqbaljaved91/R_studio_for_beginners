3b9ab013990e46f8015942034a88c39eb8fcf82a
Iqbal Javed <256380632+Iqbaljaved91@users.noreply.github.com>
2026-03-09 14:00:23 +0500

    Add files via upload

---
+++ video-2.R
@@ -0,0 +1,23 @@
+library(readxl)
+
+library(readxl)
+data <- read_excel("data.xlsx", col_types = c("text", 
+                                              "numeric", "numeric"))
+
+View(my_data)
+str(my_data)     # To view structure of data
+
+head(my_data)    # gives only first 6 rows to view
+tail(my_data)    # gives last 6 rows of data
+
+plot(my_data)
+boxplot(my_data)  # gives an error beacause boxplot is drawn between 2 variables
+
+boxplot(my_data$Height , my_data$Weight)    
+
+boxplot(my_data$Height , my_data$Crop)   # gives an error, x-axis or argument must comes first
+
+boxplot(my_data$Crop ,my_data$Height)  # error, because there is different function for boxplot between two different variable types
+
+
+boxplot(my_data$Height ~ my_data$Crop)
