b434fde0b431f199eaca40c93a74aa63cef17c8c
Iqbal Javed <256380632+Iqbaljaved91@users.noreply.github.com>
2026-03-10 21:55:59 +0500

    Add files via upload

---
+++ video-11.R
@@ -0,0 +1,25 @@
+#Sequence and Repeats in R
+
+seq(from= 0, to=100)
+
+
+seq(from= 1, to=121)
+seq(1,121)
+
+seq(1, 100, by=5)
+seq(0,100, by=10)
+seq1 <- seq(3, 33, by=1.3)
+seq1
+
+#rep
+
+rep("hello", times=3)
+rep("hello", 3)
+
+rep(123, times=100)
+rep("Homework", 100)
+rep(1:10, each=2)
+rep(1:10, each=3)
+rep(1:10, each=3, times=2)
+rep("I Love R with Aammar", 100)
+
